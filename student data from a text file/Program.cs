802281b53a579a525833a3f7d371b311fb497696
Jambukesh <jkesh360@gmail.com>
2023-11-21 19:20:57 +0530

    student data

---
+++ student data from a text file/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Collections.Specialized.BitVector32;
+using System.Diagnostics;
+
+namespace student_data_from_a_text_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string filePath = "D:\\mphasis\\problem statement\\simply learn projects\\section 6 ,1.14/student_data.txt";
+
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                Console.WriteLine("Rainbow School Students Data:");
+
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
